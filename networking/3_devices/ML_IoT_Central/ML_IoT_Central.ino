aba2a80806897f29eaa117c1b5eea589bd3b0673
Simon Erlandsson <kontakt@simonerlandsson.se>
2022-11-29 23:13:37 +0100

    Added communication for three devices, but stalls at times...

---
+++ networking/3_devices/ML_IoT_Central/ML_IoT_Central.ino
@@ -0,0 +1,118 @@
+#include <ArduinoBLE.h>
+
+#define CENTRAL_ID 1
+
+typedef struct __attribute__( ( packed ) )
+{
+  uint8_t turn;
+  int16_t w0;
+  int16_t w1;
+  int16_t w2;
+} weights_data_t;
+
+weights_data_t weightsData;
+
+unsigned long last_read_time = 0;
+
+void listenWeights(BLEDevice peripheral) {
+  // connect to the peripheral
+  Serial.println("Connecting ...");
+
+  if (peripheral.connect()) {
+    Serial.println("Connected");
+  } else {
+    Serial.println("Failed to connect!");
+    return;
+  }
+
+  // discover peripheral attributes
+  Serial.println("Discovering attributes ...");
+  if (peripheral.discoverAttributes()) {
+    Serial.println("Attributes discovered");
+  } else {
+    Serial.println("Attribute discovery failed!");
+    peripheral.disconnect();
+    return;
+  }
+
+  // retrieve the Weights characteristics
+  BLECharacteristic readCharacteristic = peripheral.characteristic("19b10001-e8f2-537e-4f6c-d104768a1214");
+  BLECharacteristic writeCharacteristic = peripheral.characteristic("19b10001-e8f2-537e-4f6c-d104768a1215");
+
+  if (!readCharacteristic || !writeCharacteristic) {
+    Serial.println("Peripheral does not have WeightsCharacteristic characteristic!");
+    peripheral.disconnect();
+    return;
+  }
+  if (!readCharacteristic.subscribe()) {
+    Serial.println("Subscription failed!");
+    peripheral.disconnect();
+    return;
+  }
+  while (peripheral.connected()) {
+    unsigned long now = millis();
+    if (readCharacteristic.valueUpdated() || now - last_read_time > 500) {
+      last_read_time =
+      readCharacteristic.readValue((uint8_t *)&weightsData, sizeof(weightsData));
+      Serial.print("Central Weights Value: ");
+      Serial.print(weightsData.turn);
+      Serial.print(" ");
+      Serial.print(weightsData.w0);
+      Serial.print(" ");
+      Serial.print(weightsData.w1);
+      Serial.print(" ");
+      Serial.println(weightsData.w2);
+      if (weightsData.turn == CENTRAL_ID) {
+        weightsData.turn = 0;
+  
+        Serial.println("Updating weights and sending to master");
+        weightsData.w1++;
+        weightsData.w2++;
+        //delay(185);
+        writeCharacteristic.writeValue((uint8_t *)&weightsData, sizeof(weightsData), true);
+      }
+    }
+  }
+  Serial.println("Peripheral disconnected");
+}
+
+void setup() {
+  Serial.begin(9600);
+  //while (!Serial);
+
+  // initialize the BLE hardware
+  BLE.begin();
+
+  Serial.println("BLE Central - Weights control");
+
+  // start scanning for peripherals
+  BLE.scanForUuid("19b10000-e8f2-537e-4f6c-d104768a1214");
+}
+
+void loop() {
+  // check if a peripheral has been discovered
+  BLEDevice peripheral = BLE.available();
+
+  if (peripheral) {
+    // discovered a peripheral, print out address, local name, and advertised service
+    Serial.print("Found ");
+    Serial.print(peripheral.address());
+    Serial.print(" '");
+    Serial.print(peripheral.localName());
+    Serial.print("' ");
+    Serial.print(peripheral.advertisedServiceUuid());
+    Serial.println();
+
+    if (peripheral.localName() != "MLMaster") {
+      return;
+    }
+
+    // stop scanning
+    BLE.stopScan();
+
+    listenWeights(peripheral);
+
+    // peripheral disconnected, start scanning again
+    BLE.scanForUuid("19b10000-e8f2-537e-4f6c-d104768a1214");
+  }
+}
---
+++ networking/3_devices/ML_IoT_Peripheral/ML_IoT_Peripheral.ino
@@ -0,0 +1,109 @@
+#include <ArduinoBLE.h>
+
+#define NBR_CENTRALS 2
+
+typedef struct __attribute__( ( packed ) )
+{
+  uint8_t turn;
+  int16_t w0;
+  int16_t w1;
+  int16_t w2;
+} weights_data_t;
+
+weights_data_t weightsData;
+
+BLEService weightsService("19B10000-E8F2-537E-4F6C-D104768A1214");  // BLE Weights Service
+
+BLECharacteristic readCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1214", BLERead | BLENotify, sizeof(weightsData));
+BLECharacteristic writeCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1215", BLEWrite, sizeof(weightsData));
+
+const int ledPin = LED_BUILTIN;  // pin to use for the LED
+
+void ConnectHandler(BLEDevice central) {
+  // central connected event handler
+  Serial.print("Connected event, central: ");
+  Serial.println(central.address());
+  BLE.advertise();
+  readCharacteristic.writeValue((uint8_t *)&weightsData, sizeof(weightsData));
+  
+}
+
+void DisconnectHandler(BLEDevice central) {
+  // central disconnected event handler
+  Serial.print("Disconnected event, central: ");
+  Serial.println(central.address());
+  BLE.advertise();
+}
+
+void setup() {
+  Serial.begin(9600);
+  //while (!Serial);
+
+  // set LED pin to output mode
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, HIGH);
+
+  // begin initialization
+  if (!BLE.begin()) {
+    Serial.println("starting BLE failed!");
+
+    while (1)
+      ;
+  }
+  BLE.setEventHandler(BLEConnected, ConnectHandler);
+  BLE.setEventHandler(BLEDisconnected, DisconnectHandler);
+  // set advertised local name and service UUID:
+  BLE.setLocalName("MLMaster");
+  BLE.setAdvertisedService(weightsService);
+
+  // add the characteristic to the service
+  weightsService.addCharacteristic(readCharacteristic);
+  weightsService.addCharacteristic(writeCharacteristic);
+
+  // add service
+  BLE.addService(weightsService);
+  // set the initial value for the characeristic:
+  weightsData.turn = 1;
+  readCharacteristic.writeValue((uint8_t *)&weightsData, sizeof(weightsData));
+  writeCharacteristic.writeValue((uint8_t *)&weightsData, sizeof(weightsData));
+
+  // start advertising
+  BLE.advertise();
+
+  Serial.println("BLE Weights Peripheral");
+}
+
+int lastTurn = NBR_CENTRALS;
+unsigned long old_time;
+
+void loop() {
+  unsigned long current_time = millis();
+  /*if (current_time - old_time > 1000) {
+    weightsCharacteristic.writeValue((uint8_t *)&weightsData, sizeof(weightsData), true);
+    old_time = current_time;
+  }*/
+  BLE.poll();
+      if (writeCharacteristic.written()) {
+      //old_time = current_time;
+      // Below can be used to read new weights and applying them to current weights, for now we just copy
+      weightsData = *((weights_data_t *)writeCharacteristic.value());
+      //weightsCharacteristic.readValue((uint8_t *)&weightsData, sizeof(weightsData)); // Also works, but copies directly
+      Serial.print("Peripheral Weights Value: ");
+      Serial.print(weightsData.turn);
+      Serial.print(" ");
+      Serial.print(weightsData.w0);
+      Serial.print(" ");
+      Serial.print(weightsData.w1);
+      Serial.print(" ");
+      Serial.println(weightsData.w2);
+  
+      if (weightsData.turn == 0) {
+        weightsData.turn = (++lastTurn - 1) % NBR_CENTRALS + 1;
+        weightsData.w0++;
+        weightsData.w2--;
+        //delay(123);
+        readCharacteristic.writeValue((uint8_t *)&weightsData, sizeof(weightsData), true);
+        Serial.println("Updating weights");
+      }
+    }
+}
